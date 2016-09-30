984225d59b6d2f95408c96d7106e78cbf8027934
Fausto Pérez <faustoperez@gmail.com>
2016-09-30 09:54:12 +0200

    Finished Data Types lesson

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,31 +2,78 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- // $("#main").append("Fausto");
+var bio = {
+	"name": "John Doe",
+	"role": "Developer",
 
+	"contacts": {
+		"mobile": "555-555-555",
+		"email": "john@example.com",
+		"github": "johndoe",
+		"twitter": "@johndoe",
+		"location": "San Francisco"
+	},
 
- // var awesomeThoughts = "I am Fausto and I am AWESOME!";
+	"welcomeMessage": "Lorem ipsum dolor sit amet",
+	"bioPic": "images/fry.png",
+	
+	"skills": [
+		"awesomeness",
+		"JS",
+		"wadus",
+		"another thing"
+	]
+}
 
- // console.log(awesomeThoughts);
+var work = {
+	"jobs": [
+		{
+			"position": "Developer",
+			"employer": "Wadus",
+			"yearsWorked": 5,
+			"workCity": "San Francisco"
+		},
+		{
+			"position": "Developer",
+			"employer": "Other employer",
+			"yearsWorked": 1,
+			"workCity": "San Francisco"
+		}
+	]
+}
 
- // var email = "fausto@udacity.com";
+var projects = {
 
- // var newEmail = email.replace("udacity", "gmail");
+	"projects": [
 
- // console.log(email);
- // console.log(newEmail);
+		{
+			"title": "Lorem",
+			"dates": "2010-2016",
+			"description": "Lorem ipsum dolor sit amet",
+			"images": "images/197x148.gif"
+		},
+		{
+			"title": "Other project",
+			"dates": "2010-2016",
+			"description": "Lorem ipsum dolor sit amet",
+			"images": "images/197x148.gif"
+		}
+	]
+}
 
- // var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
- // console.log(funThoughts);
-
- // $("#main").append(funThoughts);
-
-
-var name = "Fausto";
-var role = "Designer";
-
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-$("#header")
- 	.append(formattedName)
- 	.append(formattedRole);
+var education = {
+	"schools": [
+		{
+			"name": "Udacity",
+			"city": "San Francisco",
+			"degree": "BA",
+			"major": ["Lorem", "Ipsum"]
+		},
+		{
+			"name": "Udacity2",
+			"city": "San Francisco2",
+			"degree": "BA2",
+			"major": ["Lorem2", "Ipsum2"]
+		}
+	]
+}
